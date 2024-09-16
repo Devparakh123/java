aa8a1890b34e7481122e93051d7fcb6d347f3707
Devparakh123 <110655937+Devparakh123@users.noreply.github.com>
2024-09-16 19:26:09 +0530

    Add files via upload

---
+++ primitve.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class primitve {
+    public static void main(String[] args) {
+        // int rollno = 64 ;
+        // char name = 'd';
+        // float  marks = 98.67f;
+        // double largedecimalNumbers = 355878.57;
+        // long largeinteger = 34433333333333L;
+        // boolean check =  false ;
+        // System.out.println(rollno);
+        // System.out.println(marks);
+
+        Scanner input  = new Scanner(System.in);
+        int rollno = input.nextInt();
+        System.out.println("it is my roll no :"+ rollno);
+    }
+}
---
+++ sum.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class sum {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        float input1 = input.nextFloat();
+        float input2 = input.nextFloat();
+
+        float sum = input1 + input2 ;
+        System.out.println(sum);
+    }
+}
